0cf7e09574a5fe8951f108f6bdce5148884e35b7
Brian Sadler <briansadler@macbook-pro-3.local>
2018-09-13 08:07:37 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Brian Sadler on 9/13/18.
+//  Copyright © 2018 Brian Sadler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
